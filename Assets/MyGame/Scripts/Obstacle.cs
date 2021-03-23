cd21aec67ce3224b70ed9b2d0ef1f8668fc8e801
VanessaStoellinger <vanessa.stoellinger@htl-salzburg.ac.at>
2021-03-23 10:22:36 +0100

    refac: delete comments

--- Assets/MyGame/Scripts/Obstacle.cs
+++ Assets/MyGame/Scripts/Obstacle.cs
@@ -10,15 +10,15 @@ private void Awake()
     rb = GetComponent<Rigidbody2D>();
 }
 
-// Update is called once per frame
+
 void Update()
 {
-    //if obstacle's position x is < -15f it will be destroyed
+   
     if(transform.position.x < -15f)
     {
         Destroy(gameObject);
 }
-        //if obstacle's position x is < -15f it will be destroyed
+
         if (transform.position.x > 15f)
         {
             Destroy(gameObject);
